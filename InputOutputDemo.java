255389ef0fddbd725c0126d60a197df26b0cb7c5
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2026-02-01 22:23:28 +0530

    [ADDED]: shubham This code for Input Output Function Demo

---
+++ InputOutputDemo.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+class InputOutputDemo{
+
+	public static void main(String args[]){
+
+		System.out.println("Hello Java ! ");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println(a);	
+
+	}
+}
