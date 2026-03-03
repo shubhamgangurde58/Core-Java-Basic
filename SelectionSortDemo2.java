f773a3e656ef995801244237ce506f4532eb33eb
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2026-03-03 23:46:09 +0530

    [ADDED]: shubham This code for SelectionSort Demo2

---
+++ SelectionSortDemo2.java
@@ -0,0 +1,40 @@
+class SelectionSortDemo2{
+
+	public static void printArray(int arr[]){
+
+		for(int i=0;i<arr.length;i++){
+
+			System.out.print(arr[i] + " ");
+
+		}
+
+	}
+
+	public static void main (String args[]){
+
+		int arr[]={7,2,9,4,6,1,3,5,10,50,80,60};
+
+		for(int i=0;i<arr.length-1;i++){
+
+			int smallest = i;
+
+			for(int j=i+1;j<arr.length;j++){
+
+				if(arr[smallest] > arr[j]){
+
+					smallest = j;
+
+				}
+
+			}
+
+			int temp = arr[smallest];
+			arr[smallest] = arr[i];
+			arr[i] = temp;
+
+		}
+
+		printArray(arr);
+
+	}
+}
